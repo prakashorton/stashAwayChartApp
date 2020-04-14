8d5de493bf7c8eeafb94a48cfe644ae161558ac8
Prakash R <prakash007kr@gmail.com>
2020-04-14 21:22:59 +0530

    Add files via upload

---
+++ precache-manifest.567bf0d0852081a1b0707287076413ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9b7914e5bc9ea9b44c1aeac07f9113",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bed40847b356004b42f1",
+    "url": "/static/css/2.f3f0ba98.chunk.css"
+  },
+  {
+    "revision": "bf1fd7de7c171c8965b7",
+    "url": "/static/css/main.e7437728.chunk.css"
+  },
+  {
+    "revision": "bed40847b356004b42f1",
+    "url": "/static/js/2.10892d12.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.10892d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1fd7de7c171c8965b7",
+    "url": "/static/js/main.a01b4e8a.chunk.js"
+  },
+  {
+    "revision": "a784b25d2956af1ff0d0",
+    "url": "/static/js/runtime-main.392dc92a.js"
+  }
+]);
